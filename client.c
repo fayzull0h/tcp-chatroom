1472e27538647d0de87c4387553d1579adc1aeb3
fayzull0h <fayzulloh2004@gmail.com>
2023-08-22 15:28:34 -0400

    Added basic window prototype

--- client.c
+++ client.c
@@ -1,6 +1,9 @@
 #include "commonutils.h"
+#include <ncurses.h>
 
 #define NAME_SIZE 32
+
+#define WINPUT_HEIGHT 6
 
 void * send_msg(void * arg);
 void * recv_msg(void * arg);
@@ -8,17 +11,18 @@ void * recv_msg(void * arg);
 char name[NAME_SIZE] = "[DEFAULT]";
 
 int main(int argc, char * argv[]) {
-    int socketFD;
-    struct sockaddr_in serv_addr;
-    pthread_t recv_th, send_th;
-    void * thread_return;
-
+    /* Check input */
     if (argc != 4) {
         printf("Usage: %s <IP> <PORT NUMBER> <USERNAME>\n", argv[0]);
         handle_error("[ERROR]: Improper usage!");
     }
-
     strcpy(name, argv[3]);
+
+    /* Set up variables for sockets and threads */
+    int socketFD;
+    struct sockaddr_in serv_addr;
+    pthread_t recv_th, send_th;
+    void * thread_return;
 
     /* Get socket file descriptor */
     socketFD = socket(AF_INET, SOCK_STREAM, 0);
@@ -31,17 +35,38 @@ int main(int argc, char * argv[]) {
     serv_addr.sin_port = htons(atoi(argv[2]));
 
     /* Attempt to connect to the server */
-    if (connect(socketFD, (const struct sockaddr*)&serv_addr, sizeof(serv_addr)) != 0)
-        handle_error("[ERROR]: Could not connect to the server!");
+    if (connect(socketFD, (const struct sockaddr*)&serv_addr, sizeof(serv_addr)) != 0) printf("hi");
+        //handle_error("[ERROR]: Could not connect to the server!");
     printf("Connected!\n");
 
-    /* Listen for messages and print (on new thread) */
+    /* NCURSES START */
+    int scrwidth, scrheight;
+    initscr();
+    getmaxyx(stdscr, scrheight, scrwidth);
+
+    WINDOW * input_win = newwin(WINPUT_HEIGHT, scrwidth-4, scrheight-WINPUT_HEIGHT-1, 2);
+    WINDOW * msgs_win = newwin(scrheight-WINPUT_HEIGHT-3, scrwidth-4, 1, 2);
+    box(input_win, 0, 0);
+    box(msgs_win, 0, 0);
+    
+    wmove(stdscr, scrheight-WINPUT_HEIGHT-2, 3);
+    printw("Enter message:");
+
+    refresh();
+    wrefresh(msgs_win);
+    wmove(input_win, 1, 1);
+    wrefresh(input_win);
+    getch();
+
+    /* Listen for messages and print (on new thread) 
     pthread_create(&recv_th, NULL, recv_msg, (void *)&socketFD);
     pthread_create(&send_th, NULL, send_msg, (void *)&socketFD);
     pthread_join(recv_th, &thread_return);
-    pthread_join(send_th, &thread_return);
+    pthread_join(send_th, &thread_return);*/
 
     close(socketFD);
+
+    endwin();
     return 0;
 }
 
